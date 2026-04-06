33da4bbe383f00cf905f19379f1ac4ae9634e184
Brian Pugnali <brian@pugnoni.us>
2026-04-06 09:31:42 -0500

    Add play round function

--- js/script.js
+++ js/script.js
@@ -15,3 +15,39 @@ function getHumanChoice() {
     return prompt("Enter rock, paper, or scissors", "rock");
 }
 
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice().toLowerCase;
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    switch (computerChoice) {
+        case (computerChoice === humanChoice):
+            alert(`Computer chose ${computerChoice}. It's a tie.`);
+            break;
+        case (computerChoice === "rock" && humanChoice === "paper"):
+            alert("Computer chose rock. You win!");
+            humanScore++;
+            break;
+        case (computerChoice === "rock" && humanChoice === "scissors"):
+            alert("Computer chose rock. Computer wins.");
+            computerScore++;
+            break;
+        case (computerChoice === "paper" && humanChoice === "rock"):
+            alert("Computer chose paper. Computer wins.");
+            computerScore++;
+            break;
+        case (computerChoice === "paper" && humanChoice === "scissors"):
+            alert("Computer chose paper. You win!");
+            humanScore++;
+            break;
+        case (computerChoice === "scissors" && humanChoice === "rock"):
+            alert("Computer chose scissors. You win!");
+            humanScore++;
+            break;
+        case (computerChoice === "scissors" && humanChoice === "paper"):
+            alert("Computer chose scissors. Computer wins.");
+            computerScore++;
+            break;
+    }
+}
