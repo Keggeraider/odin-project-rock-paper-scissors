30051a5ada0105bcda9acc0e9df3125dc8501831
Brian Pugnali <brian@pugnoni.us>
2026-04-19 08:17:57 -0500

    Refactor code and add scoreboard

--- js/script.js
+++ js/script.js
@@ -1,83 +1,87 @@
-const playGame = () => {
-    
-    let humanScore = 0;
-    let computerScore = 0;
-    const computerChoice = () => {
-        const choice = Math.floor(Math.random() * 3);
+let humanScore = 0;
+let computerScore = 0;
 
-        if (choice === 0){
-            return "rock";
-        } else if (choice === 1){
-            return "paper";
-        } else {
-            return "scissors";
-        }
-    };
+const playRound = (human, computer) => {
 
-    const humanChoice = () => {
-        let choice = prompt("Enter rock, paper, or scissors", "rock").toLowerCase().trim();
-        
-        while ((choice !== "rock") && (choice !== "paper") && (choice !== "scissors")) {
-            choice = prompt("Enter rock, paper, or scissors", "rock").toLowerCase().trim();
-        }
-
-        return choice;
-    };
-
-    const playRound = (human, computer) => {
-
-        switch (computer) {
-            case human:
-                alert(`Computer chose ${computer}. It's a tie.`);
-                break;
-            case "rock":
-                switch (human) {
-                    case "paper":
-                        alert("Computer chose rock. You win!");
-                        humanScore++;
-                        break;
-                    case "scissors":
-                        alert("Computer chose rock. Computer wins.");
-                        computerScore++;
-                        break;
-                }
-                break;
-            case "paper":
-                switch (human) {
-                    case "rock":
-                        alert("Computer chose paper. Computer wins.");
-                        computerScore++;
-                        break;
-                    case "scissors":
-                        alert("Computer chose paper. You win!");
-                        humanScore++;
-                        break;
-                }
-                break;
-            case "scissors":
-                switch (human) {
-                    case "rock":
-                        alert("Computer chose scissors. You win!");
-                        humanScore++;
-                        break;
-                    case "paper":
-                        alert("Computer chose scissors. Computer wins.");
-                        computerScore++;
-                        break;
-                }
-                break;
-        }
-    };
-
-    for (i = 0; i < 5; i++ ) {
-
-        playRound(humanChoice(), computerChoice());
-        
+    switch (computer) {
+        case human:
+            alert(`Computer chose ${computer}. It's a tie.`);
+            break;
+        case "rock":
+            switch (human) {
+                case "paper":
+                    alert("Computer chose rock. You win!");
+                    humanScore++;
+                    break;
+                case "scissors":
+                    alert("Computer chose rock. Computer wins.");
+                    computerScore++;
+                    break;
+            }
+            break;
+        case "paper":
+            switch (human) {
+                case "rock":
+                    alert("Computer chose paper. Computer wins.");
+                    computerScore++;
+                    break;
+                case "scissors":
+                    alert("Computer chose paper. You win!");
+                    humanScore++;
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (human) {
+                case "rock":
+                    alert("Computer chose scissors. You win!");
+                    humanScore++;
+                    break;
+                case "paper":
+                    alert("Computer chose scissors. Computer wins.");
+                    computerScore++;
+                    break;
+            }
+            break;
     }
+};
+       
+const computerChoice = () => {
+    const choice = Math.floor(Math.random() * 3);
 
-    alert(`Computer Score is ${computerScore} and your score is ${humanScore}`);
-    (computerScore > humanScore) ? alert("Computer Wins.") : alert("You win!");
+    if (choice === 0){
+        return "rock";
+    } else if (choice === 1){
+        return "paper";
+    } else {
+        return "scissors";
+    }
 };
 
-playGame();
+const btns = document.querySelector(".buttons");
+let humanChoice = "";
+let computerPick = "";
 
+btns.addEventListener('click', (e) => {
+    let target = e.target;
+
+    humanChoice = target.id;
+    computerPick = computerChoice();
+
+    // playRound(humanChoice, computerPick);
+
+    console.log(`human: ${humanChoice}, computer: ${computerPick}`);
+
+});
+
+ 
+
+    // for (i = 0; i < 5; i++ ) {
+
+    //     playRound(humanChoice(), computerChoice());
+        
+    // }
+
+
+    // alert(`Computer Score is ${computerScore} and your score is ${humanScore}`);
+    // (computerScore > humanScore) ? alert("Computer Wins.") : alert("You win!");
